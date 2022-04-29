fd41978cd8d482c64077e45717861d4f674624cd
firatkill <firatkill@icloud.com>
2022-04-29 10:01:17 +0300

    done
    without easy/hard levels

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  changeAll();
+  randomQuestion(6);
+});
+let level = "easy";
+document.querySelector(".row-cols-3").addEventListener("click", control);
+document.querySelector("#newColors").onclick = () => {
+  changeAll();
+};
+
 function randomRGB() {
   return `rgb(${(Math.random() * 255).toFixed(0)},${(
     Math.random() * 255
@@ -9,25 +19,37 @@ function randomColors(name) {
   document.querySelector(`${name}`).style.backgroundColor = `${random}`;
   document.querySelector(`${name}`).style.color = `${random}`;
 }
-changeAll();
+
 function changeAll() {
   for (let i = 1; i < 7; i++) {
     randomColors(`#color${i}`);
   }
+  randomQuestion(6);
 }
-document.addEventListener("click", control);
+let wrongAnswer = 0;
+
 function control(e) {
   if (
-    e.target.style.backgroundColor !=
+    e.target.style.backgroundColor ==
     document.querySelector("#question").textContent.toLowerCase()
   ) {
-    console.log("naber");
+    document.querySelector("#score").textContent++;
+    if (document.querySelector("#score").textContent == 10) {
+      document.querySelector("#score").textContent = "YOU WON!";
+      document.querySelector("#score").style.backgroundColor = "green";
+    }
+    changeAll();
+  } else {
+    wrongAnswer++;
+    if (wrongAnswer == 3) {
+      document.querySelector("#score").textContent--;
+      wrongAnswer = 0;
+    }
   }
 }
 
-function randomQuestion() {
+function randomQuestion(param) {
   document.querySelector("#question").textContent = document
-    .getElementById(`color${(Math.random() * 6).toFixed(0)}`)
+    .getElementById(`color${(Math.random() * param).toFixed(0)}`)
     .style.backgroundColor.toUpperCase();
 }
-randomQuestion();
